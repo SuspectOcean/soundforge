e31ba34ce857f77cf5b536cb2dd405c9a368450e
SuspectOcean <roscoebloomfield89@gmail.com>
2026-03-24 09:05:06 -0400

    Update route.ts

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,24 +1,3 @@
-import { NextAuthOptions } from "next-auth";
-import { GitHubProvider } from "next-auth/providers/github";
-import { GoogleProvider } from "next-auth/providers/google";
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import { prisma } from "@/lib/prisma";
+import { handlers } from "@/lib/auth";
 
-export const authOptions: NextAuthOptions = {
-  adapter: PrismaAdapter(prisma),
-  providers: [
-    GitHubProvider({
-      clientId: process.env.NEXTAUTH_GITHUB_ID || "",
-      clientSecret: process.env.NEXTAUTH_GITHUB_SECRET || "",
-    }),
-    GoogleProvider({
-      clientId: process.env.NEXTAUTH_GOOGLE_ID || "",
-      clientSecret: process.env.NEXTAUTH_GOOGLE_SECRET || "",
-    }),
-  ],
-  callbacks: {
-    signIn: {account} => true,
-    jwt: {token} => true,
-  },
-  custom: {},
-};
+export const { GET, POST } = handlers;
